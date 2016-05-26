994bab4852a2e57419c27a9de1f295481ea633ae
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-05-26 14:42:46 +0800

    Now the relevent area is colored

--- server.R
+++ server.R
@@ -5,8 +5,12 @@ server <- function(input, output){
   output$plot1 <-  renderPlot({
     x <- seq(-15,15, by = 0.1)
     y <- dnorm(x, mean = input$mean, sd = input$sd)
+    xpol <- seq(input$x, 15, length=100)
+    ypol <- dnorm(xpol, input$mean, input$sd)
+    xpolval <- rep(0,100)
     plot(x,y, type = "l", col = "red", xlim = c(-15,15))
     abline( v = input$x, col = "blue")
+    polygon( c(xpol, rev(xpol)), c(xpolval, rev(ypol)), col = "blue")
   })
   
   output$pvalue <- renderText({
