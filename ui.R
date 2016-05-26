5e7639527a65bee50c5b60f5a61f12a5df362048
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-05-26 18:06:38 +0800

    added navigation panel

--- ui.R
+++ ui.R
@@ -1,24 +1,34 @@
 library(shiny)
 
 ui <- fluidPage(
-  fluidRow(
-
-    fluidRow(
-      column(4, numericInput( 'mean', "Mean: ", value = 2, min = -10, max = 10)),
-      column(4, numericInput( 'sd', "SD: ", value = 1, min = 1, max = 5)),
-      column(4, numericInput( 'x' , "x Value:", value = 0, min = -10, max = 10))
-    ),
+  
+  navbarPage("Distribution",
+             navlistPanel(
+               tabPanel("Normal Distribution", 
+                        fluidRow(
+                          column(4, numericInput( 'mean', "Mean: ", value = 2, min = -10, max = 10)),
+                          column(4, numericInput( 'sd', "SD: ", value = 1, min = 1, max = 5)),
+                          column(4, numericInput( 'x' , "x Value:", value = 0, min = -10, max = 10))),
+                        
+                        fluidRow(
+                          column(12, plotOutput("plot1"))),
+                        
+                        
+                        fluidRow(
+                          column(12, textOutput("pvalue")),
+                          column(12, p('use the following formula to get the pvalue:')),
+                          column(12, textOutput("formula1")))
+                        
+               ),
+               
+               tabPanel("Binomial Distribution"),
+               
+               tabPanel("Central Limit Theory"))
+             
+  )    
     
-    fluidRow(
-      column(12, plotOutput("plot1"))
-    ),
-    
-    fluidRow(
-      column(12, textOutput("pvalue")),
-      column(12, p('use the following formula to get the pvalue:')),
-      column(12, textOutput("formula1"))
-    )
   )
   
-)
+  
+  
 
