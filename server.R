7590db93979bfe828c074687d063c3f0c50bd725
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-05-26 12:54:03 +0800

    Added ab line and its corresponding pvalue

--- server.R
+++ server.R
@@ -6,6 +6,12 @@ server <- function(input, output){
     x <- seq(-15,15, by = 0.1)
     y <- dnorm(x, mean = input$mean, sd = input$sd)
     plot(x,y, type = "l", col = "red", xlim = c(-15,15))
+    abline( v = input$x, col = "blue")
+  })
+  
+  output$pvalue <- renderText({
+    pval <- pnorm(input$x, mean = input$mean, sd = input$sd)
+    paste("The probability is:", round(pval, 3), sep = " ")
   })
   
 }
--- ui.R
+++ ui.R
@@ -4,12 +4,17 @@ ui <- fluidPage(
   fluidRow(
 
     fluidRow(
-      column(6, numericInput( 'mean', "Mean: ", value = 2, min = -10, max = 10)),
-      column(6, numericInput( 'sd', "SD: ", value = 1, min = 1, max = 5))
+      column(4, numericInput( 'mean', "Mean: ", value = 2, min = -10, max = 10)),
+      column(4, numericInput( 'sd', "SD: ", value = 1, min = 1, max = 5)),
+      column(4, numericInput( 'x' , "x Value:", value = 0, min = -10, max = 10))
     ),
     
     fluidRow(
       column(12, plotOutput("plot1"))
+    ),
+    
+    fluidRow(
+      column(12, textOutput("pvalue"))
     )
   )
   
